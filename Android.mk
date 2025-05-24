f8f720456afbf2c6a932e504c89c827d8d036b55
Arian <arian.kulmer@web.de>
2025-05-24 12:33:20 +0200

    marble: Initial import from OS2.0.4.0.VMRMIXM
    Matching https://github.com/LineageOS/android_device_xiaomi_marble/tree/f3f393920a8a61a0522b51bde31e3c97568d8d6d

---
+++ Android.mk
@@ -0,0 +1,30 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),marble)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,a5630b1d1c73e9d2bf3e6fccd3821bb3591fcadc)
+$(call add-radio-file-sha1-checked,radio/aop.img,58f99b06291b589b79154545b560b687c7e88c71)
+$(call add-radio-file-sha1-checked,radio/aop_config.img,e7aab6cba28fb1ea2061bbf0ea130f6a71c15acb)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,684b5e4b8e2104d706281a27104ad7800d44bb0e)
+$(call add-radio-file-sha1-checked,radio/cpucp.img,209e738da54bcfac4106297f30ddde93707c5087)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,10d1bd59b4cfa7a7cd5ec578d5f4f98f5d72e309)
+$(call add-radio-file-sha1-checked,radio/dsp.img,abb0cf432db0d513601b5314b55b1765dc900317)
+$(call add-radio-file-sha1-checked,radio/featenabler.img,8b4ea8f4339224723a1a746d1c84b03e851aedd9)
+$(call add-radio-file-sha1-checked,radio/hyp.img,76e947aaeb49bbcf822ecd67b78045795106a39e)
+$(call add-radio-file-sha1-checked,radio/imagefv.img,90b5b896f12ffe5bdf4705ac54b4406e8e238390)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,6624e34c6ad143f8685b0a9a53d513a8702e3b2a)
+$(call add-radio-file-sha1-checked,radio/modem.img,1eb3e6bb97501942b7c41c6823f54e4f53beb53c)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,7ee6c4dc510a2dfaff13b057fa444322a10f390f)
+$(call add-radio-file-sha1-checked,radio/shrm.img,9469e19a70e27175a8b9b0938c04771733b32494)
+$(call add-radio-file-sha1-checked,radio/tz.img,ef53dcecf2d62d6e2464f87bf1338069390c7765)
+$(call add-radio-file-sha1-checked,radio/uefi.img,5edb42d3bf823d690fb3b32ebc374fdaa2d34bee)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,64a95018cde29b37efa22152fe08c805304c2774)
+$(call add-radio-file-sha1-checked,radio/xbl.img,acfaf1e65a256491a520073bfa1531b8a5c852b3)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,7aa3669371c947b870938fc34982e0ac344574c4)
+$(call add-radio-file-sha1-checked,radio/xbl_ramdump.img,c33da37c903a58f5964d477a539c130c126ccccc)
+
+endif
