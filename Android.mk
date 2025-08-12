f56a90e8be50c65ce1bd319b94f5f7802266db0e
SharmagRit <sharmagritstuff@gmail.com>
2025-08-12 15:18:47 +0000

    marble: Update blobs from marble OS2.0.203.0.VMRMIXM
    Signed-off-by: SharmagRit <sharmagritstuff@gmail.com>

---
+++ Android.mk
@@ -0,0 +1,30 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),marble)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,1e8383eab83fef7f8401010371f5443a7dfb018d)
+$(call add-radio-file-sha1-checked,radio/aop.img,b553407c4534a86eea97df7d45244fbfb43f4da5)
+$(call add-radio-file-sha1-checked,radio/aop_config.img,b7ea54443dd25352d3469a351e2df0d0e18d7df9)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,edb70bd1dc9bf0e48918eeede23f7e6cd2cd781d)
+$(call add-radio-file-sha1-checked,radio/cpucp.img,aa2dfd989895debbd8ebd8877bb73a40ef0d11da)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,4aecc736925c81123d3b2ee99e1a1dc25c91602a)
+$(call add-radio-file-sha1-checked,radio/dsp.img,abb0cf432db0d513601b5314b55b1765dc900317)
+$(call add-radio-file-sha1-checked,radio/featenabler.img,7ecbb432f8a00626096108b57d4a5743be8359f5)
+$(call add-radio-file-sha1-checked,radio/hyp.img,acdd7507d142c18cd527973d8d9370c89a5262b7)
+$(call add-radio-file-sha1-checked,radio/imagefv.img,706112f2a81f239df76d6181bc099ea0f017423e)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,45d8a9427118697c7a579fb60341f63afb314a01)
+$(call add-radio-file-sha1-checked,radio/modem.img,7fd83efa00166cefd18aa9b7e5f3fed520b94f7e)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,d7b73e7af61c7b3db1adca04736bc5d76794dcf4)
+$(call add-radio-file-sha1-checked,radio/shrm.img,5492de51631efd2e014bddf88e39202823ba3554)
+$(call add-radio-file-sha1-checked,radio/tz.img,20b05b95c20b2315fd94d6482e39de5afe80781f)
+$(call add-radio-file-sha1-checked,radio/uefi.img,18d44e4aaf96c267fc4a0a1f4d3ec9dcfb78d844)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,95a2fc4797dc08e26dfaeea1748a1fdfa1126199)
+$(call add-radio-file-sha1-checked,radio/xbl.img,5a62b7ad661afd4151086c7519eec56dad337267)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,b4a32e98faf99b48ffaa412dff9d2380a361f1af)
+$(call add-radio-file-sha1-checked,radio/xbl_ramdump.img,aee3a6c9447e9f90a5887299ccb3523312543857)
+
+endif
